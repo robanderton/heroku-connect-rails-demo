df8ae8745a0412f4fe1fd0f2107986fc8d1baf2f
Rob Anderton <randerton@heroku.com>
2016-06-13 12:34:37 +0100

    use SQL format for database dumps

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module HerokuConnectRailsDemo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Use native schema (to support triggers and extensions)
+    config.active_record.schema_format = :sql
   end
 end
